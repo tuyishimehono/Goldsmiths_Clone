0bbf9835104a1be661ee6ad8c81fe1564bcf45bc
tuyishimehono <tuhonori1@gmail.com>
2024-05-06 15:34:56 +0200

    Adding Website Clone Files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'collage-bg': "url('/img/collage_desktop.png')",
+      }
+    },
+  },
+  plugins: [],
+}
+
